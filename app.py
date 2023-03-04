2a43db40e8700d49b82df85fe83460dbef9c1aba
Liubomyr Chumak <63431742+LiubomyrChumak@users.noreply.github.com>
2023-03-04 15:09:19 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+
+@app.route("/hello", methods=["GET", "POST"])
+def hello():
+    if request.method == "POST":
+        name = request.form["name"]
+        return f"Hello, {name}!"
+    else:
+        return render_template("hello.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
